8eb6e0eed954a2da546d15ac0c1378eb8a078bb8
AkihitoNishimura <plumizreop.3@gmail.com>
2020-09-06 15:58:10 +0900

    Initial Commit

---
+++ ListViewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewApp
+//
+//  Created by 西村映人 on 2020/09/06.
+//  Copyright © 2020 西村映人. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
